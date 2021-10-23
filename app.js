30e66dc61b50d2cdc5ce72b9b1fd09a0cddf38ef
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-23 17:35:14 +0600

    handle Rejected Promise Errors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('express-async-errors');
 const express = require('express');
 const userRouter = require('./routers/userRouters')
-
 const app = express()
 const cors = require('cors');
+const error = require('./middleware/error')
 
 /*if(process.env.NODE_ENV === 'development'){
 
@@ -10,5 +11,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 app.use(process.env.URL, userRouter);
+
+app.use(error);
 
 module.exports = app;
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,16 +15,12 @@ module.exports.signUp = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     const token = user.generateJWT();
-    try {
-        const result = await user.save();
-        return res.status(201).send({
-            message: "Registration Successfull!",
-            token: token,
-            user: _.pick(result, ["_id", "name", "email"])
-        })
-    } catch (error) {
-        return res.status(500).send("Something Failed!")
-    }
+    const result = await user.save();
+    return res.status(201).send({
+        message: "Registration Successfull!",
+        token: token,
+        user: _.pick(result, ["_id", "name", "email"])
+    })
 
 }
 

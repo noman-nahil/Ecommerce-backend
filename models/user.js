23d31a83b57bdd33f0debd891d847367423d664b
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-23 14:05:47 +0600

    user model

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const joi = require('joi');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+userSchema.methods.generateJWT = () => {
+    const token = jwt.sign({
+        _id: this._id,
+        email: this.email,
+        role: this.role,
+        name: this.name
+    }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+    return token;
+}
+
+const validatedUser = user => {
+    const schema = joi({
+        name: joi.string().min(3).max(100).required(),
+        email: joi.string().min(5).max(255).required(),
+        password: joi.string().min(5).max(255).required(),
+    });
+    return schema.validate(user)
+}
+module.exports.User = ('User', userSchema);
+module.exports.validate = validatedUser;
+
